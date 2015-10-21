7c6509f843a0b8b0ed7e141a800e6990f23f001b
Anthony Chen <github@anthonychen.net>
2015-10-21 15:47:08 -0400

    Change more SR font sizes.  Fix the way that we remove bold on SRs

--- prettySMART.py
+++ prettySMART.py
@@ -63,10 +63,12 @@ def NormalizeFont(f, n):
             if "font-family" in tspan.attrib and tspan.attrib["font-family"] == "Times New Roman":
                 tspan.attrib["font-family"] = "Arial"
                 
-            if (tspan.attrib["font-size"] == "24.000" or tspan.attrib["font-size"] == "23.000" or tspan.attrib["font-size"] == "22.000") and "fill" in tspan.attrib and tspan.attrib["fill"] == "#000000" and "font-family" in tspan.attrib and tspan.attrib["font-family"] == "Arial":
+            if tspan.attrib["font-size"] in ["24.000", "23.000", "22.000", "26.000"] and "fill" in tspan.attrib and tspan.attrib["fill"] == "#000000" and "font-family" in tspan.attrib and tspan.attrib["font-family"] == "Arial":
+                tspan.attrib["font-size"] = "28.000"
+
+            if tspan.attrib["font-size"] == "28.000" and "fill" in tspan.attrib and tspan.attrib["fill"] == "#000000":
                 if "font-weight" in tspan.attrib:
                     del tspan.attrib["font-weight"]
-                tspan.attrib["font-size"] = "28.000"
 
             if tspan.attrib["font-size"] == "48.000":
                 tspan.attrib["font-weight"] = "bold"
