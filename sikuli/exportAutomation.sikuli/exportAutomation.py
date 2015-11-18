31f38ccda0eb4cfb3d7c45cf50dfb1c8d72c0a99
Anthony Chen <github@anthonychen.net>
2015-11-18 10:09:38 -0500

    Add UI Automation files for exporting to PDF

---
+++ sikuli/exportAutomation.sikuli/exportAutomation.py
@@ -0,0 +1,37 @@
+waitVanish("1447777379646.png")
+
+click("1447773474193.png")
+type("f", Key.ALT)
+type("e")
+type("d")
+# Click Full Page
+click("1447771302504.png")
+# Delete the Header/Date fields
+type(Key.TAB)
+type("x", Key.CTRL)
+type(Key.TAB)
+type(Key.TAB)
+type(Key.DELETE)
+# Set to Landscape
+click("1447771403950.png")
+click("1447771417897.png")
+# Save the PDF
+type(Key.ENTER)
+wait("1447772564402.png")
+wait("1447772573994.png")
+# Enter Filename
+type("v", Key.CTRL)
+# Delete notebook from the end.
+type(Key.BACKSPACE)
+type(Key.BACKSPACE)
+type(Key.BACKSPACE)
+type(Key.BACKSPACE)
+type(Key.BACKSPACE)
+type(Key.BACKSPACE)
+type(Key.BACKSPACE)
+type(Key.BACKSPACE)
+type(Key.BACKSPACE)
+type(Key.HOME)
+type("C:\\njctl\\PDFs\\")
+# Save!
+type(Key.ENTER)
