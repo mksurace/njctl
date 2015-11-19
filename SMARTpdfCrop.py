c537c451c671a5170a617f15a8ef885652951909
Anthony Chen <github@anthonychen.net>
2015-11-19 10:38:40 -0500

    Add PDF Cropper.  Leaves a small 25px margin at the bottom for page numbers

---
+++ SMARTpdfCrop.py
@@ -0,0 +1,23 @@
+# NJCTL SMARTpdfCrop
+# anthony@njctl.org
+
+import sys
+from pyPdf import PdfFileWriter, PdfFileReader
+
+if __name__ == "__main__":
+    input1 = PdfFileReader(file(sys.argv[1], "rb"))
+    output = PdfFileWriter()
+
+    numPages = input1.getNumPages()
+
+    for i in range(numPages):
+        page = input1.getPage(i)
+        page.trimBox.lowerLeft = (88, 25)
+        page.trimBox.upperRight = (753, 548)
+        page.cropBox.lowerLeft = (88, 25)
+        page.cropBox.upperRight = (753, 548)
+        output.addPage(page)
+
+    outputStream = file("%s-cropped.pdf" % sys.argv[1], "wb")
+    output.write(outputStream)
+    outputStream.close()
