42b998fe0c1b06e63b942ffa3e958a63edcddea5
Anthony Chen <github@anthonychen.net>
2016-01-20 14:21:47 -0500

    initial checkin of pdf splicer

---
+++ pdfSplicer.py
@@ -0,0 +1,25 @@
+import argparse
+from pyPdf import PdfFileWriter, PdfFileReader
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Splice a PDF')
+    parser.add_argument('pdfPath', help='path to PDF')
+    parser.add_argument('topic', help='topic name')
+    parser.add_argument('start', help='start of range, inclusive', type=int)
+    parser.add_argument('end', help='end of range, inclusive', type=int)
+
+    args = parser.parse_args()
+
+    input1 = PdfFileReader(file(args.pdfPath, "rb"))
+    output = PdfFileWriter()
+
+    numPages = input1.getNumPages()
+
+    for i in range(args.start, args.end):
+        page = input1.getPage(i)
+        output.addPage(page)
+
+    outputStream = file(args.pdfPath.replace(".pdf", ".%s.%d-%d.pdf" %(args.topic, args.start, args.end)), "wb")
+    output.write(outputStream)
+    outputStream.close()
+
