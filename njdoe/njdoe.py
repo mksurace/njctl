8ee88ffacc92f2e656fb69ff5752dd72f8b9c780
Anthony Chen <github@anthonychen.net>
2016-01-22 16:18:23 -0500

    Add more data to the NJDOE work

--- njdoe/njdoe.py
+++ njdoe/njdoe.py
@@ -10,6 +10,15 @@ trueYearMap = {
     14: 2014
     }
 
+def handleInt(s):
+    s = s.strip()
+    if s == "*":
+        s = None
+    if s:
+        return int(s)
+    else:
+        return 0
+
 def handleData(s):
     s = s.strip()
     if s == "*":
@@ -22,9 +31,12 @@ def handleData(s):
 SHOULDDOWNLOAD = False
 
 with open("masterfile.txt", 'w') as o:
-    o.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" %
+    o.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" %
         ("Year",
          "Grade",
+         "CDS",
+         "CountyName",
+         "DistrictName",
          "SchoolName",
          "TotalValidScaleMath",
          "TotalPPMath",
@@ -37,7 +49,9 @@ with open("masterfile.txt", 'w') as o:
          "TotalAPScience",
          "TotalMeanScaleScience",
          "MathRank",
-         "ScienceRank"
+         "ScienceRank",
+         "MathPct",
+         "SciencePct"
          ))
 
 for yr in [2010, 2011, 2012, 2013, 13, 14]:
@@ -53,19 +67,22 @@ for yr in [2010, 2011, 2012, 2013, 13, 14]:
                 # NJDOE state summaries use fixed width formatting.
                 schoolname = l[109:159].strip()
                 if schoolname:
-                    TotalValidScaleMath = handleData(l[232:238])
+                    schoolCDS = l[0:9]
+                    countyName = l[9:59].strip()
+                    districtName = l[59:109].strip()
+                    TotalValidScaleMath = handleInt(l[232:238])
                     TotalPPMath = handleData(l[238:242])
                     TotalPMath = handleData(l[242:246])
                     TotalAPMath = handleData(l[246:250])
                     TotalMeanScaleMath = handleData(l[250:254])
 
-                    TotalValidScaleScience = handleData(l[278:284])
+                    TotalValidScaleScience = handleInt(l[278:284])
                     TotalPPScience = handleData(l[284:288])
                     TotalPScience = handleData(l[288:292])
                     TotalAPScience = handleData(l[292:296])
                     TotalMeanScaleScience = handleData(l[296:300])
 
-                    schools.append([schoolname, TotalValidScaleMath,
+                    schools.append([schoolCDS, countyName, districtName, schoolname, TotalValidScaleMath,
                                  TotalPPMath,
                                  TotalPMath,
                                  TotalAPMath,
@@ -78,31 +95,67 @@ for yr in [2010, 2011, 2012, 2013, 13, 14]:
 
             schoolScienceRankings = {}
             schoolMathRankings = {}
+            schoolMathPct = {}
+            schoolSciencePct = {}
 
             # Sort by total mean scale math
-            schools.sort(key=lambda tup: tup[5], reverse=True)
+            schools.sort(key=lambda tup: tup[8], reverse=True)
 
-            rank = 0
+            mathCount = 0
+            rank = 1
+            curScore = 0
             for school in schools:
-                if school[5] > 0:
+                if school[8] > 0:
                     schoolMathRankings[school[0]] = rank
-                    rank = rank + 1
+
+                    if curScore != school[8]:
+                        rank = rank + 1
+                        curScore = school[8]
+                        
+                    mathCount = mathCount + 1
+                    
+                    worseSchoolCount = 0
+                    for i in range(mathCount, len(schools)):
+                        if schools[i][8] > 0 and schools[i][8] < curScore:
+                            worseSchoolCount = worseSchoolCount + 1
+                    schoolMathPct[school[0]] = worseSchoolCount
                 else:
                     schoolMathRankings[school[0]] = -1
+                    schoolMathPct[school[0]] = -1
 
             # Sort by total mean scale science
-            schools.sort(key=lambda tup: tup[10], reverse=True)
-            rank = 0
+            schools.sort(key=lambda tup: tup[13], reverse=True)
+            scienceCount = 0
+            rank = 1
+            curScore = 0
             for school in schools:
-                if school[10] > 0:
+                if school[13] > 0:
                     schoolScienceRankings[school[0]] = rank
-                    rank = rank + 1
+
+                    if curScore != school[13]:
+                        rank = rank + 1
+                        curScore = school[13]
+                        
+                    scienceCount = scienceCount + 1
+
+                    worseSchoolCount = 0
+                    for i in range(scienceCount, len(schools)):
+                        if schools[i][13] > 0 and schools[i][13] < curScore:
+                            worseSchoolCount = worseSchoolCount + 1
+                    schoolSciencePct[school[0]] = worseSchoolCount
                 else:
                     schoolScienceRankings[school[0]] = -1
+                    schoolSciencePct[school[0]] = -1
                     
             with open("masterfile.txt", 'a') as o:
                 for school in schools:
-                    o.write("%d\t%d\t%s\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%d\t%d\n" %
+                    mathPct = -1
+                    sciencePct = -1
+                    if schoolMathPct[school[0]] > 0:
+                        mathPct = 100*(1-float(schoolMathPct[school[0]])/mathCount)
+                    if schoolSciencePct[school[0]] > 0:
+                        sciencePct = 100*(1-float(schoolSciencePct[school[0]])/scienceCount)
+                    o.write("%d\t%d\t%s\t%s\t%s\t%s\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%0.1f\t%d\t%d\t%0.1f\t%0.1f\n" %
                         (trueYearMap[yr], gr, school[0],
                          school[1],
                          school[2],
@@ -114,7 +167,12 @@ for yr in [2010, 2011, 2012, 2013, 13, 14]:
                          school[8],
                          school[9],
                          school[10],
+                         school[11],
+                         school[12],
+                         school[13],
                          schoolMathRankings[school[0]],
-                         schoolScienceRankings[school[0]]
+                         schoolScienceRankings[school[0]],
+                         mathPct,
+                         sciencePct
                          ))
                 
